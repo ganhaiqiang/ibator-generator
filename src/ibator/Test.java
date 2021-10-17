0d581905add49d52fb94044363ff21b52220294e
GHQ <ganhaiqiang@aliyun.com>
2021-10-17 15:15:22 +0800

    生成mysql和oracle驱动

--- src/ibator/Test.java
+++ src/ibator/Test.java
@@ -4,12 +4,6 @@ import java.util.Date;
 
 import org.apache.ibatis.ibator.util.ClassNameUtils;
 
-/**
- *
- *
- * @author QQ:34847009
- * @date 2010-11-4 下午03:02:26
- */
 public class Test {
 
 	public static void main(String[] args) {
